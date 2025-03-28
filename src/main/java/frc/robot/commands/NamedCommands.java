a1a79a6e0bf779309f83c8d26ad3c944587711a5
Aforementioned1 <113794557+Aforementioned1@users.noreply.github.com>
2025-03-28 17:26:53 -0500

    Update at 'Fri Mar 28 17:26:53 CDT 2025'

--- src/main/java/frc/robot/commands/NamedCommands.java
+++ src/main/java/frc/robot/commands/NamedCommands.java
@@ -70,8 +70,8 @@ public class NamedCommands {
             () ->
                 objective
                     .map(FieldConstants.CoralObjective::reefLevel)
-                    .orElse(FieldConstants.ReefLevel.L4))
-        .andThen(elevator.setPositionBlocking(elevHeight, Seconds.of(10000)))
+                    .orElse(FieldConstants.ReefLevel.L4)).withTimeout(Seconds.of(6))
+        .andThen(elevator.setPositionBlocking(elevHeight, Seconds.of(10000))) // maybe should be 10? (was the measurement originally in ms?)
         .andThen(Commands.waitSeconds(0.1))
         .andThen(intake.outtakeUntilSensor(() -> 0.3))
         .andThen(elevator.setPositionBlocking(() -> 1, Seconds.of(1000)));
