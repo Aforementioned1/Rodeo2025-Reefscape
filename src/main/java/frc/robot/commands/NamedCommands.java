bbdc3188332ec3b3196dfed1b08bda9670d38f7c
Aforementioned1 <113794557+Aforementioned1@users.noreply.github.com>
2025-03-29 13:05:41 -0500

    Update at 'Sat Mar 29 13:05:41 CDT 2025'

--- src/main/java/frc/robot/commands/NamedCommands.java
+++ src/main/java/frc/robot/commands/NamedCommands.java
@@ -71,7 +71,7 @@ public class NamedCommands {
                 objective
                     .map(FieldConstants.CoralObjective::reefLevel)
                     .orElse(FieldConstants.ReefLevel.L4))
-        .withTimeout(Seconds.of(5) /* stop auto aligning after 5 seconds*/)
+        .withTimeout(Seconds.of(3) /* stop auto aligning after 5 seconds*/)
         .andThen(
             elevator.setPositionBlocking(
                 elevHeight,
