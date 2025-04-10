8ec5b06e6c35fb5a1f965b0be4dbc7250bb4e834
Aforementioned1 <113794557+Aforementioned1@users.noreply.github.com>
2025-04-10 18:24:09 -0500

    Update at 'Thu Apr 10 18:24:09 CDT 2025'

--- src/main/java/frc/robot/commands/NamedCommands.java
+++ src/main/java/frc/robot/commands/NamedCommands.java
@@ -86,7 +86,7 @@ public class NamedCommands {
         .andThen(
             elevator.setPositionBlocking(
                 elevHeight,
-                Seconds.of(1.5))) // maybe should be 10? (was the measurement originally in ms?)
+                Seconds.of(10000.0))) // maybe should be 10? (was the measurement originally in ms?)
         .andThen(Commands.waitSeconds(0.05))
         .andThen(intake.outtakeUntilSensor(() -> 0.3))
         .andThen(elevator.setPositionBlocking(() -> 1, Seconds.of(1000)));
